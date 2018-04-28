1019427ac4a46de74608f2b4b9c2a5cea62130c8
romalms10 <romalms10@gmail.com>
2018-04-28 09:26:14 -0700

    Delete unnecessary files

--- betty.py
+++ betty.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 import subprocess
-import os
 from sys import argv
 
 if len(argv) < 2:
     print('Usage: betty FILE1 <FILE2...>')
     exit()
 
-cwd = os.getcwd()
 args = [ 'sudo', 'docker-compose', 'exec', 'betty', 'betty']
 for arg in argv[1:]:
     args.append(arg)
