2f2b13054705050ef7872ae8cfc49117c1697817
romalms10 <romalms10@gmail.com>
2018-04-28 15:33:50 -0700

    setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # Initialization script
 
-git clone https://github.com/eightlimbed/betty-docker.git
-cd betty-docker
-sudo cp docker-compose.yml /docker-compose.yml
-sudo cp betty /usr/bin/
-sudo cp pep8 /usr/bin/
-sudo cp shellcheck /usr/bin/
-sudo docker-compose up -d -f /docker-compose.yml
+cp docker-compose.yml /
+cp betty /usr/bin/
+cp pep8 /usr/bin/
+cp shellcheck /usr/bin/
+docker-compose -f /docker-compose.yml build
+docker-compose -f /docker-compose.yml up -d
