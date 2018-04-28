57c8e750565fa59767c7f9b9d5aa365cbe72a3bc
Lee Gaines <eightlimbed@gmail.com>
2018-04-28 15:49:03 -0700

    updates setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # Initialization script
 
-git clone https://github.com/eightlimbed/betty-docker.git
-cd betty-docker
 sudo cp docker-compose.yml /docker-compose.yml
+cd / && sudo docker-compose up -d
 sudo cp betty /usr/bin/
 sudo cp pep8 /usr/bin/
 sudo cp shellcheck /usr/bin/
-sudo docker-compose up -d -f /docker-compose.yml
