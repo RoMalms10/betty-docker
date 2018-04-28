09c23cd877850321b924e7db017944ca519f2835
Lee Gaines <eightlimbed@gmail.com>
2018-04-28 05:10:33 +0000

    updates setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Initialization script
 
-sudo cp docker-compose.yml /docker-compose.yml
-cd / && sudo docker-compose up -d
+docker-compose build
+docker-compose up -d
 sudo cp betty /usr/bin/
 sudo cp pep8 /usr/bin/
 sudo cp shellcheck /usr/bin/
